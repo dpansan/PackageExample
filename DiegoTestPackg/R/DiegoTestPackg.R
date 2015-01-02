12aa6c313fb0711bb7cb00f56d4f05f311b066db
Diego Paniagua <dpansan@gmail.com>
2015-01-02 21:01:29 +0100

    Commmit inicial

---
+++ DiegoTestPackg/R/DiegoTestPackg.R
@@ -0,0 +1,9 @@
+#'Cuadrado de un número
+#'
+#'Eleva cualquier nujmero al cuadrado
+#'@param Valor numérico pasado a la función
+#'@return El cuadrado del número pasado como parámetro
+#'@export 
+cuadrado<-function(x){
+  return (x^2)
+}
